a5cec4542434a9d3cb5080568289d747835f9e7c
Prateek <Prateekrajgrd74@gmail.com>
2022-12-25 10:56:23 +0530

    class131.py

---
+++ class131.py
@@ -0,0 +1,11 @@
+# squares=[]
+# for i in range(1,11):
+#     squares.append(i**2)
+# print(squares)    
+
+# square2 = [i**2 for i in range(1,11)]
+# print(square2)
+
+
+negative = [-i for i in range(1,11)]
+print(negative)
